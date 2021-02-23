58d3fd07bf4eaf074a3d1843c50c3b6d3ad513ee
Alessandro-Urbani <zatsenfrutsen002@gmail.com>
2021-02-23 17:59:47 +0100

    Piccole modifiche app.module.ts

--- rubricapaginatamac/src/app/app.module.ts
+++ rubricapaginatamac/src/app/app.module.ts
@@ -1,12 +1,24 @@
+import { LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { logging } from 'protractor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountPageComponent } from './count-page/count-page.component';
+import { ErrorMessagePageComponent } from './error-message-page/error-message-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountPageComponent,
+    ErrorMessagePageComponent,
+    LoginPageComponent,
+    MainPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
