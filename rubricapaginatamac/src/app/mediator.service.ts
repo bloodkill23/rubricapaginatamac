541ad2ff8e989f9615553fcd0af3694450b89c59
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-02-25 11:44:30 +0100

    modifiche html

--- rubricapaginatamac/src/app/mediator.service.ts
+++ rubricapaginatamac/src/app/mediator.service.ts
@@ -5,7 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class MediatorService {
 
-  contatti: string []=[];
+  username: string = "";
+
+
+  contatti: string[] = [];
 
   constructor() { }
 }
